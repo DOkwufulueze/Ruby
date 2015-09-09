08b28dfcff90748b57869e0c54d37ce5b18fe6be
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-09 23:36:45 +0100

    Overwrite default to_s method

---
+++ Exercise7/bin/main.rb
@@ -0,0 +1,5 @@
+require_relative "../lib/string.rb"
+
+puts "Enter text to inverse the case of each letter"
+puts gets.chomp.to_s
+
---
+++ Exercise7/lib/string.rb
@@ -0,0 +1,6 @@
+class String
+  def to_s
+    self.swapcase
+  end
+end
+
