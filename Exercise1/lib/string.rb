14ef2ba4cbbc18d73794c04c20d2fc6740d5dda1
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-09 15:53:45 +0100

    Show the number of occurrences of various alphabet letters in an input string

--- Exercise1/lib/string.rb
+++ Exercise1/lib/string.rb
@@ -1,15 +1,13 @@
 class String
   def make_hash
     occurrences = Hash.new(0)
-    self.each_char {
-      |character| occurrences[character] += 1
-    }
+    self.each_char { |character| occurrences[character] += 1 }
 
     occurrences
   end
 
   def show_occurrences
-    self.make_hash.each {|item| puts "#{item[0]} \t\t #{item[1]}"}
+    self.make_hash.each { |item| puts "#{item[0]} \t\t #{item[1]}" }
   end
 end
 
