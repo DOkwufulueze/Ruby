3892bd4730f6ae9a78ed988c43684b40a4d1854e
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-10 09:43:19 +0100

    Create array of elements raised to a certain power.

--- Exercise8/bin/main.rb
+++ Exercise8/bin/main.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/array.rb"
 
-array = [1, 2, 3, 4, 5, 6]
+puts "Enter numbers to make an array of"
+array = gets.chomp.split(/\D+/).map(&:to_i)
 puts "Enter a number to make an exponent"
 exponent = gets.chomp.to_i
 p "Elements of array #{array} raised to the power of #{exponent} produces array -> #{array.power(exponent)}"
