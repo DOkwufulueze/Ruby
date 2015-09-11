9a26c7724443f561b6d98d04d25b8f749c2254ab
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-11 16:44:57 +0100

    Show the number of occurrences of various alphabet letters in an input string

--- Exercise1/bin/main.rb
+++ Exercise1/bin/main.rb
@@ -3,6 +3,6 @@ require_relative('../lib/string.rb')
 puts "Enter text to see number of occurrences of various alphabet letters"
 text = gets.chomp.gsub(' ','')
 puts "Character \t Frequency"
-text.show_occurrences
+text.show_occurrences { |item| puts item }
 
 
--- Exercise1/lib/string.rb
+++ Exercise1/lib/string.rb
@@ -2,7 +2,7 @@ class String
   def show_occurrences
     occurrences = Hash.new(0)
     self.each_char { |character| occurrences[character] += 1 }
-    occurrences.each { |item| puts "#{item[0]} \t\t #{item[1]}" }
+    occurrences.each { |item| yield "#{item[0]} \t\t #{item[1]}" }
   end
 end
 
