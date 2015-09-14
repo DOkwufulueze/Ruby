d4b4aae8384f49242c0c1d5d2c0a4935f9503b7d
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-14 14:06:39 +0100

    Check for Palindrome

--- Exercise4/bin/main.rb
+++ Exercise4/bin/main.rb
@@ -1,5 +1,14 @@
 require_relative('../lib/palindrome_tester.rb')
 
 tester = PalindromeTester.new
-tester.enter_input
+puts 'Enter a text to test if it\'s a Palindrome '
+input = gets.chomp
+
+until input.match(/q/i)
+  puts tester.check_if_palindrome(input)
+  puts 'Enter a text to test if it\'s a Palindrome '
+  input = gets.chomp
+end
+
+abort 'Thanks for checking. BYE...'
 
