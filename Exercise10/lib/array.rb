9d19bd2865d2a1ca8d56d1517d887c0a10e9d7f9
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-15 09:15:59 +0100

    Implement inject method on Array Hash

--- Exercise10/lib/array.rb
+++ Exercise10/lib/array.rb
@@ -7,13 +7,13 @@ class Array
       hash[item.length].push(modified_item)
     end
 
-    hash.group_by { |key, value| key.even? ? "even" : "odd" }
+    hash
   end
 
   def use_inject
-    make_hash.inject(Hash.new { |hash, key| hash[key] = [] }) do |hash, (key, value)|
-      value.each {|item| hash[key].push(item[1])}
-      hash
+    make_hash.inject(Hash.new { |hash, key| hash[key] = [] }) do |resulting_hash, (source_hash_key, source_hash_value)|
+      source_hash_key.odd? ? resulting_hash["odd"].push(source_hash_value) : resulting_hash["even"].push(source_hash_value)
+      resulting_hash
     end
   end
 end
