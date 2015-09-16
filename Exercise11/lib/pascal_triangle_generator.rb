80e6f2343e66ac2df44e6c2e08b676748ffb3410
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-16 10:29:14 +0100

    Generate Pascal Triangle

--- Exercise11/lib/pascal_triangle_generator.rb
+++ Exercise11/lib/pascal_triangle_generator.rb
@@ -1,18 +1,29 @@
 class PascalTriangleGenerator
   def initialize(input)
     @input = input + 1
-    generate_pascal_triangle_for { |row_array| puts row_array.join(" ").center(50, " ") }
+    generate_pascal_triangle { |row_array| puts row_array.join(" ").center(50, " ") }
   end
 
-  def generate_pascal_triangle_for
-    if @input <= 1
-      warn
-    else
-      row_array = [1]
-      @input.times do |counter|
-        yield row_array = counter >= 1 ? row_array.unshift(0).push(0).each_cons(2).map { |first_value, second_value| first_value + second_value } : row_array
+  def generate_pascal_triangle
+    if block_given?
+      if @input <= 1
+        warn
+      else
+        row_array = [1]
+        @input.times do |counter|
+          yield row_array = counter >= 1 ? modify_row(row_array) : [1]
+        end
       end
+    else
+      puts ":::Please supply a block"
     end
+  end
+
+  def modify_row(row_array)
+    row_array.unshift(0)
+            .push(0)
+            .each_cons(2)
+            .map { |first_value, second_value| first_value + second_value }
   end
 
   def warn
