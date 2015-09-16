09f4a56cf2ffce00aba8ecb341a972dd89a6ba14
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-16 10:48:07 +0100

    Show Fibonacci sequence up to 1000

--- Exercise3/lib/fixnum.rb
+++ Exercise3/lib/fixnum.rb
@@ -1,9 +1,13 @@
 class Fixnum
   def each_fibonacci
-    pivot_number, second_number = 1, 1
-    while pivot_number <= self
-      yield pivot_number
-      pivot_number, second_number = second_number, pivot_number + second_number
+    if block_given?
+      pivot_number, second_number = 1, 1
+      while pivot_number <= self
+        yield pivot_number
+        pivot_number, second_number = second_number, pivot_number + second_number
+      end
+    else
+      puts ":::Please supply a block"
     end
   end
 end
