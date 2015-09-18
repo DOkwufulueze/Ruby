3231a611e38f37c0a0d572e96eb9d32813f2f111
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-18 09:20:48 +0100

    Difference between Compund and Simple Interests

--- Exercise19/bin/main.rb
+++ Exercise19/bin/main.rb
@@ -1,9 +1,9 @@
 require_relative "../lib/interest.rb"
 
-puts "Enter Principal and Time in the order requested (Default if you supply nothing is 1.0 for each)"
-input = gets.chomp.split(/[\s,]+/)
-principal = Float(input[0]) rescue 1.0
-time = Float(input[1]) rescue 1.0
+puts "Enter Principal (Default if you supply nothing is 1.0 for each)"
+principal = Float(gets.chomp) rescue 1.0
+puts "Enter Time (Default if you supply nothing is 1.0 for each)"
+time = Float(gets.chomp) rescue 1.0
 interest = Interest.new do |the_class|
   the_class.principal = principal
   the_class.time = time
